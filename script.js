f117263577ad742b02056cd60a7d034850488d07
ParthDawande <125895972+ParthDawande@users.noreply.github.com>
2025-11-14 10:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-//complete this code
 class Person {
-	_name;
-	_age;
-	name;
-	age;
-	constructor(name,age){
-		this._name = name;
-		this._age = age;
-		this.name = name;
-		this.age = age;
-	}
+    constructor(name, age) {
+        this._name = name;   // internal storage
+        this._age = age;     // internal storage
+    }
 
-	getName(){
-		return this._name;
-	}
+    // getter for name
+    get name() {
+        return this._name;
+    }
 
-	setAge(age){
-		this._age = age;
-		this.age=age;
-	}
+    // getter for age (optional but tests use person.age)
+    get age() {
+        return this._age;
+    }
+
+    // setter for age
+    set age(age) {
+        this._age = age;
+    }
 }
 
 class Student extends Person {
-	study(){
-		console.log(this._name+" is studying");
-	}
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(this._name+" is teaching");
-	}
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
 // Do not change the code below this line
